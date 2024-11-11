ee44cc209bc96d6d07d9355f428334a278a14cca
Vladimir Blinov <70836867+Gladfn@users.noreply.github.com>
2024-11-11 12:15:08 +0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# TOKEN = "5442708117:AAGr2uhZW5KMW-LggNKkcULQaqy8MH60QhI"
-TOKEN = "6731773259:AAE0zIIB4G2p_iYsfyrfMXpbFmpdFonpKyw"
-mysql = {
-    "user": "root",
-    "password": "",
-    "host": "127.0.0.1",
-    "database": "Users"
-}
