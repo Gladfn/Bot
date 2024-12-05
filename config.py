6f2a0908ae059f0bbeb8833290161b4ebc6d6c7d
Vladimir Blinov <70836867+Gladfn@users.noreply.github.com>
2024-12-05 18:35:40 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "" #Your bot's token
+mysql = {
+    "user": "", #The user name for the database
+    "password": "", #The password for the database
+    "host": "127.0.0.1", #IP your database
+    "database": "Users" #Name your database
+}
